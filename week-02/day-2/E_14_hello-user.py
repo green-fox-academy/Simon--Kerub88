fa1867b50a2149c67b3ffa1706353a418d9e4a94
Simon_NT <kacsa1984@gmail.com>
2017-03-21 17:36:49 +0100

    Finished E_14_hello-user.py | and proud

---
+++ week-02/day-2/E_14_hello-user.py
@@ -0,0 +1,7 @@
+# Modify this program to greet the user instead of the World!
+# The program should ask for the name of the user
+
+
+print("Add your name please")
+userName = input()
+print("Hello", userName,"!" )
