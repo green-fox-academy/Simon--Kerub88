7760c20c3fbda03527ee74d5885f2cc7bd996009
Simon_NT <kacsa1984@gmail.com>
2017-03-22 07:36:13 +0100

    first --if - else -- exercise

---
+++ week-02/day-2/E_18_odd-even.py
@@ -0,0 +1,8 @@
+# Write a program that reads a number form the standard input,
+# Than prints "Odd" if the number is odd, or "Even" it it is even.
+
+print("Enter a number")
+num = int(input())
+if num % 2 == 0:
+    print("Even")
+else: print("Odd")
