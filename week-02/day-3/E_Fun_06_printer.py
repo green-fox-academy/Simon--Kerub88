c98d14424c7db3ccfcfc79436e91f6904731caaa
Simon_NT <kacsa1984@gmail.com>
2017-03-22 16:34:00 +0100

    some unfinished exercise

--- week-02/day-3/E_Fun_06_printer.py
+++ week-02/day-3/E_Fun_06_printer.py
@@ -2,14 +2,4 @@
 #   which prints the input parameters
 #   (can have multiple number of arguments)
 
-# def printer(stuff_to_drop):
-#     print(stuff_to_drop)
-#
-# printer("Faradt vagyok")
-
-
-def printer(*args):
-    for i in range(0, len(args)):
-        print(args[i], end=" ")
-
-printer(input("Enter words: "))
+def printer(stuff_to_drop):
