4f1710693b5fe474ba27a398d5bbec01f486df65
Simon_NT <kacsa1984@gmail.com>
2017-03-22 07:53:47 +0100

    First try worked. Very proud.

---
+++ week-02/day-2/E_20_print-bigger.py
@@ -0,0 +1,9 @@
+# Write a program that asks for two numbers and prints the bigger one
+print("Enter a number. Human")
+num1 = int(input())
+print("Enter a new number. Human")
+num2 = int(input())
+
+if num1 > num2:
+    print("Mr. Bigger number is ", num1)
+else: print("Mr. Bigger number is ", num1)
