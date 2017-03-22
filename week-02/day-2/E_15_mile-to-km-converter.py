56eb22e581479c09603a09e374868257af41b00e
Simon_NT <kacsa1984@gmail.com>
2017-03-22 07:02:29 +0100

    km/mile converter, good stuff about int(input()

---
+++ week-02/day-2/E_15_mile-to-km-converter.py
@@ -0,0 +1,7 @@
+# Write a program that asks for an integer that is a distance in kilometers,
+# then it converts that value to miles and prints it
+
+print("Enter the distance in km: ")
+distance_km = int(input())
+disdistance_mile = distance_km / 1.6
+print("It's", disance_mile, "miles")
