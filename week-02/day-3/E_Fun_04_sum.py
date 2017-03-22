d4757d98b9f2034e4fab9a444425a7ff2b472e07
Simon_NT <kacsa1984@gmail.com>
2017-03-22 13:40:58 +0100

    change list element type  basic exercise

--- week-02/day-3/E_Fun_04_sum.py
+++ week-02/day-3/E_Fun_04_sum.py
@@ -1,11 +1,9 @@
 # - Write a function called `sum` that sum all the numbers
 #   until the given parameter
-
-
 def sum(inpt_list):
     basic = 0
     for i in range(1, inpt_list+1):
         basic += i
     return basic
 
-print(sum(3))
+print(sum(88))
