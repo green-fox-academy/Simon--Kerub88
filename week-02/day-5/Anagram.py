2eea6fd3fd893307bb672217fa59bbb55d0a6728
Simon_NT <kacsa1984@gmail.com>
2017-03-24 13:14:26 +0100

    Anagram v2, more complicated process but long and cluncky. It is working tho.

--- week-02/day-5/Anagram.py
+++ week-02/day-5/Anagram.py
@@ -1,12 +1,36 @@
 # Create a function named is anagram following your current language's style guide. It should take two strings and return a boolean value depending on whether its an anagram or not.
 
-def is_anagram(t1,t2):
-    if t1[::-1] == t2:
-        return True
-    else:
-        return False
 
+# Csinalok egy loopot ami t1 minden betujet ellenorzi. Aztan egy masikat ami t2 elemeit ellenorzi es vegul egy if_else-et, hogy megegyeznek e az elemei.
 
-print(is_anagram("dog", "god"))
-print(is_anagram("cbaed", "abcde"))
+# print(is_anagram("dog", "god"))
+# print(is_anagram("green", "fox"))
+
+# def is_anagram(t1, t2):
+#     t2_list = list(t2)
+#
+#     position1 = 0
+#     go_on = True
+
+# amig position1 kisebb mint a t2 karaktereinek szamma addig megy a while loop
+    while position1 < len(t1) and go_on:
+        position2 = 0
+        stopped = False
+        while position2 < len(t2_list) and not stopped:
+            if t1[position1] == t2_list[position2]:
+                stopped = True
+            else:
+                position2 = position2 + 1
+
+        if stopped:
+            t2_list[position2] = None
+        else:
+            go_on = False
+
+        position1 = position2 +1
+
+    return go_on
+
+print(is_anagram("god", "dog"))
 print(is_anagram("green", "fox"))
+print(is_anagram("abcd", "bcad"))
