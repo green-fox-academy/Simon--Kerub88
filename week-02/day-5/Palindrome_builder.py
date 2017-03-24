433b4ce9806ef51a89722279d00356f504cb92cc
Simon_NT <kacsa1984@gmail.com>
2017-03-24 13:42:09 +0100

    Palindrom_builder, solved with [::-1] reverse

---
+++ week-02/day-5/Palindrome_builder.py
@@ -0,0 +1,7 @@
+# Create a function named create palindrome. It should take a string, create a palindrome from it and then return it.
+
+def palindrom(text):
+    text = text + text[::-1]
+    return text
+
+print(palindrom("123"))
