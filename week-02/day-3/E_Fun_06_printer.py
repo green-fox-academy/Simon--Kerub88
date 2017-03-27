05a78f97cef57743305040e271cfb8160a84cf2b
Simon_NT <kacsa1984@gmail.com>
2017-03-27 07:34:11 +0200

    printer finished, used the Flexible Number of Argument

--- week-02/day-3/E_Fun_06_printer.py
+++ week-02/day-3/E_Fun_06_printer.py
@@ -2,4 +2,11 @@
 #   which prints the input parameters
 #   (can have multiple number of arguments)
 
-def printer(stuff_to_drop):
+list_of_stuff = [1, 2, 3]
+
+
+def printer(*stuff_to_drop):
+    for p in range(0, len(stuff_to_drop)):
+        print(stuff_to_drop[p], end = " ")
+
+printer(input("Enter stuff: "))
