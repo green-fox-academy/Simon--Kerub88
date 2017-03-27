f05f63a7718c003953efb369cb56f423601e18a7
Simon_NT <kacsa1984@gmail.com>
2017-03-27 07:37:09 +0200

    factorio finised with a for loop and multiplier of the loop elements

--- week-02/day-3/E_Fun_05_factorio.py
+++ week-02/day-3/E_Fun_05_factorio.py
@@ -2,19 +2,10 @@
 #   that returns it's input's factorial
 
 
-# def factorio(numbers):
-#     summa = 1
-#     for n in range(1, numbers+1):
-#         summa *= 1
-#     return summa
-#
-# print(factorio(5))
-
-
-def factorio(x):
-    ossz = 1
-    for i in range(1, x+1):
-        ossz *= i
-    return ossz
+def factorio(numbers):
+    summa = 1
+    for n in range(1, numbers+1):
+        summa *= n
+    return summa
 
 print(factorio(5))
--- week-02/day-3/E_Fun_06_printer.py
+++ week-02/day-3/E_Fun_06_printer.py
@@ -2,9 +2,6 @@
 #   which prints the input parameters
 #   (can have multiple number of arguments)
 
-list_of_stuff = [1, 2, 3]
-
-
 def printer(*stuff_to_drop):
     for p in range(0, len(stuff_to_drop)):
         print(stuff_to_drop[p], end = " ")
