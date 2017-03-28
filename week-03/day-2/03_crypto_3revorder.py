71677797a9343b0f33311a2c03bc4f2120a6c775
Simon_NT <kacsa1984@gmail.com>
2017-03-28 14:07:31 +0200

    crypto_3revorder finished. Reverted the text first, put the reverted text to a for-loop than send back to a string variable

--- week-03/day-2/03_crypto_3revorder.py
+++ week-03/day-2/03_crypto_3revorder.py
@@ -1,3 +1,12 @@
 # Create a method that decrypts texts/reversed_zen_order.txt
-def decrypt(file_name):
-    pass
+fr = open("reversed-order.txt")
+text = fr.readlines()
+
+def decrypt(text_content):
+    text_content = text_content[::-1]
+    decode_text = ""
+    for line in text_content:
+        decode_text += line
+    return decode_text
+
+print(decrypt(text))
