57d182ed76666ff18f7fa4d01f535036a63e7a61
Simon_NT <kacsa1984@gmail.com>
2017-03-28 12:00:50 +0200

    First try, but its not working

--- week-03/day-2/02_crypto_2revlines.py
+++ week-03/day-2/02_crypto_2revlines.py
@@ -1,3 +1,11 @@
 # Create a method that decrypts texts/reversed_zen_lines.txt
+fr = open("reversed-lines.txt")
+text = fr.readlines()
+
 def decrypt(file_name):
-    pass
+    decode_text = ""
+    for t in range(0, len(file_name)):
+        decode_text += file_name[-t]
+    return decode_text
+
+print(decrypt(text))
