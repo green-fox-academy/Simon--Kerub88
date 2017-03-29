baabcebe407c49cf9029d3e24d1acdfc21a77c6c
Simon_NT <kacsa1984@gmail.com>
2017-03-29 10:15:53 +0200

    First drawn funkcion. Draw line from input to the center

---
+++ week-03/day-3/04_go-to-center.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# create a line drawing function that takes 2 parameters:
+# the x and y coordinates of the line's starting point
+# and draws a line from that point to the center of the canvas.
+# draw 3 lines with that function.
+
+def line_drawn(x, y):
+    line = canvas.create_line(x, y, 150, 150)
+    return line
+
+line_drawn(50, 10)
+line_drawn(300, 300)
+line_drawn(0, 0)
+
+root.mainloop()
