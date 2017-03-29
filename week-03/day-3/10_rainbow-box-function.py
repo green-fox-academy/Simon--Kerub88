5c4ab9e09b0e1ba67576906748398e93c4e985c6
Simon_NT <kacsa1984@gmail.com>
2017-03-29 13:34:11 +0200

    rainbow box finished

---
+++ week-03/day-3/10_rainbow-box-function.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# create a square drawing function that takes 2 parameters:
+# the square size, and the fill color,
+# and draws a square of that size and color to the center of the canvas.
+# create a loop that fills the canvas with rainbow colored squares.
+
+
+
+def square_drawn(size, color):
+    size = size / 2
+    square = canvas.create_rectangle(150 - size, 150 - size, 150 + size, 150 + size, fill = color)
+
+color = ["red", "orange", "yellow", "green", "blue", "#4B0082", "#EE82EE"]
+size = 300
+for c in range(1, len(color)):
+    square_drawn(size, color[c])
+    size -= 20
+
+
+
+square_drawn(140, "red")
+# square_drawn(100)
+# square_drawn(50)
+
+root.mainloop()
