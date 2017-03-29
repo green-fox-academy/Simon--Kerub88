403bb535864d295f589f0b4a236aa68eb6099614
Simon_NT <kacsa1984@gmail.com>
2017-03-29 17:52:33 +0200

    feat(chessboard grid)

--- week-03/day-3/04_go-to-center.py
+++ week-03/day-3/04_go-to-center.py
@@ -1,7 +1,7 @@
 # create a line drawing function that takes 2 parameters:
 # the x and y coordinates of the line's starting point
 # and draws a line from that point to the center of the canvas.
-# draw 3 lines with that function.
+# draw 3 lines with that function.s
 
 from tkinter import *
 
--- week-03/day-3/13_checkerboard.py
+++ week-03/day-3/13_checkerboard.py
@@ -1,23 +1,48 @@
-from tkinter import *
-
-root = Tk()
-
-canvas = Canvas(root, width='300', height='300')
-canvas.pack()
-
 # fill the canvas with a checkerboard pattern.
 
-def chess_lines(x1, x2, y1, y2):
-    square = canvas.create_rectangle(x1, x2, y1, y2)
+from tkinter import *
 
-for i in range(8):
-    x1 = 0
-    x2 = 0
-    y1 = 37.5
-    y2 = 37.5
-    chess_lines(x1 += i * 37.5, x2, y1 += i * 37.5, y2 += i * 37.5)
-    # x1 += i * 37.5
-    # y1 += i * 37.5
-    # y2 += i * 37.5
+def grid_maker(canvas, line_distance):
+    for x in range(line_distance,canvas_width,line_distance):
+       canvas.create_line(x, 0, x, canvas_height, fill="#476042")
+    for y in range(line_distance,canvas_height,line_distance):
+       canvas.create_line(0, y, canvas_width, y, fill="#476042")
+
+root = Tk()
+canvas_width = 300
+canvas_height = 300
+w = Canvas(root,
+            width = canvas_width,
+            height = canvas_height)
+w.pack()
+
+grid_maker(w, 37.5)
+
+# canvas = Canvas(root, width='300', height='300')
+# canvas.pack()
+
+
+
+
+
+
+
+
+
+
+# def chess_lines(x1, y2, cell_size):
+#     square = canvas.create_rectangle(x1, x2, y1, y2)
+#
+# x1 = 0
+# x2 = 0
+# y1 = 37.5
+# y2 = 37.5
+#
+# for i in range(8):
+#
+#     chess_lines(x1, x2, y1, y2)
+#     x1 += i * 37.5
+#     y1 += i * 37.5
+#     y2 += i * 37.5
 
 root.mainloop()
