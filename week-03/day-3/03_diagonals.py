0efe88d1d1e7ffb7188b705396bcd2f57ef26891
Simon_NT <kacsa1984@gmail.com>
2017-03-29 10:09:16 +0200

    diagonal finished

---
+++ week-03/day-3/03_diagonals.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+line = canvas.create_line(0, 0, 300, 300)
+
+
+# draw the canvas' diagonals in green.
+
+root.mainloop()
