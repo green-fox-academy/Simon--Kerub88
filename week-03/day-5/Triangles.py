44c2dd4f2624b9edb6ea7ce8e5fbd17e932fe15a
Simon_NT <kacsa1984@gmail.com>
2017-03-31 11:27:05 +0200

    triangle still not working, but looks good, so i commit

--- week-03/day-5/Triangles.py
+++ week-03/day-5/Triangles.py
@@ -5,19 +5,19 @@ root = Tk()
 canvas = Canvas(root, width='600', height='560')
 canvas.pack()
 
-def draw_polygon(x, y, size):
-    canvas.create_polygon(x, y, x+size, y, size / 2, size, fill = "white", outline = "black", width=2)
+def draw_polygon(x, y, size, x2, y2, size2):
+    canvas.create_polygon(x, y, size, y, size / 2, size, fill = "#cc9933", outline = "black", width=2)
 
-def recursive_polygons(x, y, size):
-    draw_polygon(x, y, size)
-    time.sleep(0.04)
+def recursive_polygons(x, y, size, x2, y2, size2):
+    draw_polygon(x, y, size, x2, y2, size2)
+    time.sleep(0.4)
     canvas.update()
     if size > 10:
-        recursive_polygons(x, y, size/2)
-        recursive_polygons(x + size / 2, y, size/2)
-        recursive_polygons(x + size / 4, y+ size / 2, size/2)
+        recursive_polygons(x, y, x + size/2, y, x + size/4, y + size/2)
+        recursive_polygons(x + size / 2, y, x + size, y, x + size * 3/4, y + size / 2)
+        recursive_polygons(x + size / 4, y + size / 2, x + size * 3/4, y + size / 2, x + size / 2, y + size)
 
 
-recursive_polygons(20, 10, 560)
+recursive_polygons(20, 10, 560, 0, 0, 0)
 
 root.mainloop()
