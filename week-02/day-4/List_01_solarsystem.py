49bbef66f69daee862c3796f260100f0b290f0ab
Simon_NT <kacsa1984@gmail.com>
2017-04-01 11:57:58 +0200

    hewillnever solved with 3 for loop, now trying with only one

--- week-02/day-4/List_01_solarsystem.py
+++ week-02/day-4/List_01_solarsystem.py
@@ -1,25 +1,17 @@
 # Saturn is missing from the planetList
 # Insert it into the correct position
 
-# planetList = ["Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"]
-#
-#
-# Planets_with_Saturn = []
-#
-# for p in range(len(planetList)):
-#     if planetList[p] == "Jupiter":
-#         Planets_with_Saturn.append(planetList[p])
-#         Planets_with_Saturn.append("Saturn")
-#     else:
-#         Planets_with_Saturn.append(planetList[p])
-#
-# for l in Planets_with_Saturn:
-#     print(l)
-
 planetList = ["Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"]
 
-planetList.insert(5, "Saturn")
 
-planetList = ' , '.join(planetList)
+Planets_with_Saturn = []
 
-print(planetList)
+for p in range(len(planetList)):
+    if planetList[p] == "Jupiter":
+        Planets_with_Saturn.append(planetList[p])
+        Planets_with_Saturn.append("Saturn")
+    else:
+        Planets_with_Saturn.append(planetList[p])
+
+for l in Planets_with_Saturn:
+    print(l)
