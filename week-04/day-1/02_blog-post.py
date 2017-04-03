ead88d988e50281c0aed649eb54b81d10ea343c7
Simon_NT <kacsa1984@gmail.com>
2017-04-03 10:31:54 +0200

    feat(class BlogPost created. Can add variables to a new blogPost

--- week-04/day-1/02_blog-post.py
+++ week-04/day-1/02_blog-post.py
@@ -12,3 +12,16 @@
 #         A popular long-form, stick-figure-illustrated blog about almost everything.
 #     "One Engineer Is Trying to Get IBM to Reckon With Trump" titled by William Turton at "2017.03.28."
 #         Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.
+
+class BlogPost:
+
+    def __init__(self, authorName, title, text, publicationDate):
+        self.authorName = authorName
+        self.title = title
+        self.text = text
+        self.publicationDate = publicationDate
+
+
+blog1 = BlogPost("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet.", "2000.05.04")
+
+print(blog1)
