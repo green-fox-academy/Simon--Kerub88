5ea9fea867ece8211df19a927d91d50c78fc7d8f
Simon_NT <kacsa1984@gmail.com>
2017-04-05 14:34:02 +0200

    feat(anagram method ready, start testing

--- week-04/day-3/work_file.py
+++ week-04/day-3/work_file.py
@@ -46,15 +46,22 @@ class Apple:
 
 # Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
 # Create a test for that.
-inp1 = "tokyo"
-inp2 = "kyoto"
 
 
-def anagram(input1, input2):
-    if len(input1) == len(input2):
-        for i in input1:
-            for t in input2:
-                if input1[i] = input1[t]:
-                    print("yeah")
+    def anagram(self, input1, input2):
+        if len(input1) == len(input2):
+            input1 = list(input1)
+            input1.sort()
+            input2 = list(input2)
+            input2.sort()
+            if input2 == input1:
+                return True
+            else:
+                return False
+        else:
+            return False
 
-anagram(inp1, inp2)
+
+
+x = Apple("red")
+print(x.anagram("tokyo", "kyoto"))
