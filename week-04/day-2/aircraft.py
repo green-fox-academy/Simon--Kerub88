5580aaea6a9ac67dab9fbd69741b98fc07a7dba0
Simon_NT <kacsa1984@gmail.com>
2017-04-10 19:30:07 +0200

    feat: carrier class added new method to refill ammo for planes and report status

--- week-04/day-2/aircraft.py
+++ week-04/day-2/aircraft.py
@@ -6,19 +6,38 @@ class Carrier():
         self.garage = []
 
     def add_aircraft(self, aircraft_name):
-        self.garage.append(aircraft_name)
+        self.aircraft_name = aircraft_name
+        if self.aircraft_name == "F16":
+            F16 = Aircraft("F16")
+            self.garage.append(F16)
+        elif self.aircraft_name == "F35":
+            F35 = Aircraft("F35")
+            self.garage.append(F35)
+        else:
+            print("I take only F16 or F35")
 
-    def carrier_status()
+    def fill(self):
+        if self.ammo >= 0:
+            for plane in self.garage:
+                plane.current_ammo += plane.max_ammo - plane.current_ammo
+                self.ammo -= plane.current_ammo
+        else:
+            return "The carrier is out of ammo"
 
 
-    # def refill(self):
-    #     self.ammo -= aircraft.max_ammo - aircraft.current_ammo
-    #     aircraft.current_ammo += aircraft.max_ammo - aircraft.current_ammo
-    #     return self.ammo, aircraft.current_ammo
+    def carrier_status(self):
+        damage = 0
+        for plane in self.garage:
+            damage += plane.current_ammo * plane.base_damage
+        print("Aircraft count: ", str(len(self.garage)) + ", Ammor Storage: ", str(self.ammo) + ", Total damage: " + str(damage), "\n" + "Aircrafts:")
+        for plane in self.garage:
+            plane.get_status()
+
+
 
 class Aircraft(Carrier):
 
-    def __init__(self, plane_type = "F16"):
+    def __init__(self, plane_type):
         self.plane_type = plane_type
         if self.plane_type == "F16":
             self.max_ammo = 8
@@ -44,15 +63,22 @@ class Aircraft(Carrier):
     def get_status(self):
         print("Type: ", self.plane_type, ", Ammo: ", self.current_ammo, ", Base Damage: ", self.base_damage, ", All Damage: ", self.base_damage * self.current_ammo)
 
+battlestar = Carrier()
+battlestar.add_aircraft("F16")
+battlestar.add_aircraft("F16")
+battlestar.add_aircraft("F16")
+battlestar.add_aircraft("F35")
+battlestar.add_aircraft("F35")
+battlestar.add_aircraft("F35")
+print(battlestar.carrier_status())
+battlestar.fill()
+print(battlestar.carrier_status())
 
-air1_F16 = Aircraft()
+
+
+# air1_F16 = Aircraft()
 # air1_F16.get_status()
 # air1_F16.refill()
 # air1_F16.get_status()
 # print(air1_F16.fight())
 # air1_F16.get_status()
-
-
-battlestar = Carrier()
-battlestar.add_aircraft(air1_F16)
-print(battlestar.garage)
