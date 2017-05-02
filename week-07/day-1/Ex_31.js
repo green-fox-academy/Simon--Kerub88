0e5240cb5f1992d9501eefd0a295f8c2a1698c4f
Simon_NT <kacsa1984@gmail.com>
2017-05-02 14:00:10 +0200

    Ex_31 finished

--- week-07/day-1/Ex_31.js
+++ week-07/day-1/Ex_31.js
@@ -1,3 +1,4 @@
+'use strict';
 var lineCount = 6;
 
 
@@ -13,3 +14,19 @@ var lineCount = 6;
 // %%%%%
 //
 // The square should have as many lines as lineCount is
+
+var emptySpace = ' ';
+var draw = '%';
+
+for (var i = 1; i <= lineCount; i++) {
+    if (i === 1 || i === lineCount) {
+
+        console.log(draw.repeat(lineCount));
+
+    }else if (1 > i < lineCount) {
+
+        console.log('%' + emptySpace.repeat(i - 2) + '%' + emptySpace.repeat(lineCount - i - 1) + '%');
+        
+    }
+
+}
