f65d2b77dc721eb360fa96b2aacd889cbf4203fe
Simon_NT <kacsa1984@gmail.com>
2017-05-02 23:06:10 +0200

    06_printer finished

---
+++ week-07/day-2/06_printer.js
@@ -0,0 +1,12 @@
+'use strict';
+// - Create a function called `printer`
+//   which logs to the console the input parameters
+//   (can have multiple number of arguments)
+
+function printer(input) {
+
+    console.log(input)
+
+}
+
+printer(6)
