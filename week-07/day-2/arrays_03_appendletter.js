254755bb801798cf4ef3f4c0f270ef40a110064a
Simon_NT <kacsa1984@gmail.com>
2017-05-03 14:50:27 +0200

    arrays_appendletter finished, used .map

---
+++ week-07/day-2/arrays_03_appendletter.js
@@ -0,0 +1,10 @@
+'use strict';
+// Add "a" to every string in far
+
+var far = ["kuty", "macsk", "kacs", "rók", "halacsk"];
+
+far = far.map(function(i){
+    return i + 'a'
+})
+
+console.log(far);
