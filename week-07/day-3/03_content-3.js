487a0d47fae58fc6f69166425e27efff24539e73
Simon_NT <kacsa1984@gmail.com>
2017-05-04 15:11:03 +0200

    Ex04 finished. .innerHTML and .innerText. used,  IZI-PIZI

---
+++ week-07/day-3/03_content-3.js
@@ -0,0 +1,7 @@
+//   fill output1 with the first paragraph's content, text only.
+//   fill output2 with the first paragraph's content keeping the cat strong.
+
+var output1 = document.querySelector('p').innerText;
+console.log(output1)
+var output2 = document.querySelector('p').innerHTML;
+console.log(output2)
