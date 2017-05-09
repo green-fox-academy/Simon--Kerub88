f8fa3593c800af70899dbbeea1d64b4a30362f7b
Simon_NT <kacsa1984@gmail.com>
2017-05-09 16:20:33 +0200

    feat(06_sharpie): sharpieConstructor first version finished

---
+++ week-08/day-2/06_sharpie.js
@@ -0,0 +1,23 @@
+// Create a Sharpie constructor
+//
+// We should know about each sharpie their color (which should be a string), width (which will be a floating point number), inkAmount (another floating point number)
+// When creating one, we need to specify the color and the width
+// Every sharpie is created with a default 100 as inkAmount
+// We can use() the sharpie objects
+// which decreases inkAmount by the width
+
+let sharpieConstructor = function(color, width, inkAmount) {
+    // this.color = color;
+    // this.width = width;
+    // this.inkAmount = inkAmount;
+
+    let sharpie = {
+        color: color,
+        width: width,
+        inkAmount: inkAmount,
+    }
+    return sharpie
+}
+
+let sharpieGreen = sharpieConstructor('green', 0.4, 100);
+console.log(sharpieGreen);
