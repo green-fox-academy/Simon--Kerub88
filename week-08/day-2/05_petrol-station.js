9e3794bcd30786795f22db8a392ee353b24d8abe
Simon_NT <kacsa1984@gmail.com>
2017-05-09 15:54:58 +0200

    exercise 05 finished, 2 different object and they called each others functions

--- week-08/day-2/05_petrol-station.js
+++ week-08/day-2/05_petrol-station.js
@@ -5,14 +5,38 @@
 //  - It should have a refill(amount) method, that increments the petrol level,
 //    than returns how much petrol it consumed from the given amount
 //  - Initialize the petrol level to zero and the capacity to 50
-//
+
+const car = {
+    petrolLevel: 0,
+    petrolCapacity: 50,
+    refill: function(amount){
+        if (this.petrolLevel + amount > this.petrolCapacity) {
+            let filledPetrol = this.petrolCapacity - this.petrolLevel
+            console.log('Car filled with ' + filledPetrol + ' liter of petrol.')
+            this.petrolLevel += this.petrolCapacity - this.petrolLevel;
+            return filledPetrol
+        } else {
+            let currentPetrolLevel = this.petrolLevel;
+            this.petrolLevel += amount;
+            let filledPetrol = this.petrolLevel - currentPetrolLevel;
+            console.log('Car filled with ' + filledPetrol + ' liter of petrol.')
+            return filledPetrol
+        }
+    }
+}
+
 // Create an object called station
 //  - It should store petrol amount called petrolStorage
 //  - It should have a provide(car) method that calls the refill method of the car
 //    with the stored petrol amount as a parameter, then decrement the used petrol
 //  - Initialize the petrol amount to 3000
 
-
+const station = {
+    petrolStorage: 3000,
+    provide: function(car) {
+        this.petrolStorage -= car.refill(this.petrolStorage);
+    }
+}
 
 
 
