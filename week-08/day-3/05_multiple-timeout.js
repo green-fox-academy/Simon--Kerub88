52fa9ba17b4c6cd816c49ef1a884dcd36656d06b
Simon_NT <kacsa1984@gmail.com>
2017-05-10 13:22:33 +0200

    finished ex_05, used setTimeout multiple times

--- week-08/day-3/05_multiple-timeout.js
+++ week-08/day-3/05_multiple-timeout.js
@@ -5,3 +5,24 @@
 // pear -> after 1 seconds
 // melon -> after 3 seconds
 // grapes -> after 5 seconds
+
+function apple(){
+    console.log('apple');
+}
+
+function pear(){
+    console.log('pear');
+}
+
+function melon(){
+    console.log('melon');
+}
+
+function grapes(){
+    console.log('grapes');
+}
+
+apple();
+setTimeout(melon, 3000);
+setTimeout(pear, 1000);
+setTimeout(grapes, 5000);
